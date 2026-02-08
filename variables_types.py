99d9277d45bf2c1c81c5be64824bf3e24cc93400
Six10J <sixten00j@gmail.com>
2026-02-08 10:54:49 +0100

    Created a program to learn about variables and types

---
+++ variables_types.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#This code is written with the help of the tutorial from Learn Python (https://www.learnpython.org/en/Variables_and_Types, 2026-02-08)
+
+#How to define a integer
+myint = 7
+print(myint)
+
+#How to define a floating point number
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print (myfloat)
+
+#How to define a string
+text = "Some Text"
+print(text)
+
+#How to use simple operators on numbers and strings
+three = 3
+six = (three + three)
+print (six)
+
+hello = "Hello,"
+world = "World!"
+hello_world = (hello + " " + world)
+print (hello_world)

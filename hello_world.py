5f4044b0980ac8ce948cc2759f64ebb8c69da4e6
Six10J <sixten00j@gmail.com>
2026-02-08 10:28:40 +0100

    Created a Hello, World program

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#This code is written with the help of the tutorial from Learn Python (https://www.learnpython.org/en/Hello%2C_World%21, 2026-02-08)
+
+#Prints out Hello, World!
+print("Hello, World!")
+
+#Code to show how Python handles code blocks with indentation
+x = 1
+if x == 1:
+    print("x is 1")
